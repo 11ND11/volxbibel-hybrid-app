f018d232c8d492f4064c2329fbd0a664773ef3fa
Andreas Steiger <hallo@andreassteiger.de>
2018-08-29 09:31:53 +0200

    [WIP][FEATURE] - sharing

--- www/js/app.js
+++ www/js/app.js
@@ -62,6 +62,27 @@ var mainView = app.views.create('.view-main');
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function () {
     console.log("Device is ready!");
+
+    // this is the complete list of currently supported params you can pass to the plugin (all optional)
+    var options = {
+        message: 'share this', // not supported on some apps (Facebook, Instagram)
+        subject: 'the subject', // fi. for email
+        files: ['', ''], // an array of filenames either locally or remotely
+        url: 'https://www.website.com/foo/#bar?a=b',
+        chooserTitle: 'Pick an app', // Android only, you can override the default share sheet title,
+        appPackageName: 'com.apple.social.facebook' // Android only, you can provide id of the App you want to share with
+    };
+
+    var onSuccess = function(result) {
+        console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+        console.log("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+    };
+
+    var onError = function(msg) {
+        console.log("Sharing failed with message: " + msg);
+    };
+
+    window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
 });
 
 // Detail page
@@ -203,19 +224,15 @@ function updateWikiText($contentContainer, book, chapter = '1') {
             if($(this).hasClass('active')) {
                 $(this).removeClass('active');
                 $(this).addClass('animated');
-                setTimeout(function() {
-                    $(this).removeClass('animated');
-                    console.log('in');
-                }, 1000);
                 $contentContainer.find('.action-button').remove();
             } else {
                 $contentContainer.find('p').removeClass('active');
                 $contentContainer.find('.action-button').remove();
-                $(this).addClass('animated');
-                setTimeout(function() {
-                    $(this).removeClass('animated');console.log('in');
-                }, 1000);
                 $(this).addClass('active').prepend('<a class="action-button button button-circle">' + shareIcon + '</a>');
+
+                setTimeout(function() {
+                    $('.action-button').addClass('scale');
+                }, 100);
             }
         });
 
